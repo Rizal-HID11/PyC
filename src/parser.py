41b55c448afd8eb6bdc42ff13ddaa3b7060b8ae2
RizalW-Dev <rizalhidayat679@gmail.com>
2025-10-19 11:36:29 +0700

    Add comments to parser.py for clarity

--- src/parser.py
+++ src/parser.py
@@ -2,15 +2,14 @@ from lexer import Token
 
 class ASTNode:
     def __init__(self, ntype, value=None, line=None):
-        self.ntype = ntype
-        self.value = value
-        self.line = line
-        self.children = []
+        self.ntype = ntype          # Node type (e.g., print, expr)
+        self.value = value          # Optional value of the node
+        self.line = line            # Line number in source code
+        self.children = []          # Child nodes for tree structure
 
 def parse(tokens):
-    ast = []
+    ast = []                       # List to store AST nodes
     for t in tokens:
-        if t.ttype == "print":
+        if t.ttype == "print":     # Handle print statements
             ast.append({"type": "print", "value": t.value})
-    return ast
-
+    return ast                     # Return the constructed AST
