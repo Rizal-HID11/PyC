5c283cda63cefd621c3dd7761edffd790e9d3c5e
RizalW-Dev <rizalhidayat679@gmail.com>
2025-10-19 11:35:30 +0700

    Clarify comments and remove unused import
    Updated comments for clarity and removed unused import.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-# from token_reader import read_token_defs   <-- hapus ini
+# main.py files the core where to start run the code.
 from lexer import lex_file
 from parser import parse
 from runner import run
@@ -7,7 +7,7 @@ import os
 def main():
     root = os.getcwd()
     pyc_file = os.path.join(root, "examples", "hello.pyc")
-    tokens = lex_file(pyc_file, [])  # token_defs tidak dipakai
+    tokens = lex_file(pyc_file, [])  # token_defs not used
     ast = parse(tokens)
     run(ast)
 
